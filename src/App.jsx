b365f0271bf083edc336779c92a3afff12948de8
reactnenja <reactnenja@gmail.com>
2024-04-06 16:24:59 +0100

    codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Dashboards from "./pages/Dashboards";
-import Login from "./pages/Login";
-import NavbarRight from "./components/navbar/NavbarRight";
-import UserTable from "./components/table/Table";
-import AddingUser from "./components/addinguser/AddingUser";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <NavbarRight />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboards" element={<Dashboards />} />
-        <Route path="/table" element={<UserTable />} />
-        <Route path="/adduser" element={<AddingUser />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-};
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/pages/Dashboards.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dashboards = () => {
-  return <div>Dashboards</div>;
-};
-
-export default Dashboards;
